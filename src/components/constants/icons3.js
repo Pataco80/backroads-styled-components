b6bdbea5008098c722cc7f4b59c246fc3ee2771e
Ricardo Do Vale <pataco80@gmail.com>
2023-06-13 17:59:43 +0200

    add logo and all icons

---
+++ src/components/constants/icons3.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Home } from "@styled-icons/entypo/Home"
+import { Tour } from "@styled-icons/material/Tour"
+import { Blog } from "@styled-icons/fa-solid/Blog"
+import { Blog as LatestsBlogs } from "@styled-icons/icomoon/Blog"
+import { MailSend as Mail } from "@styled-icons/boxicons-regular/MailSend"
+import { FacebookSquare as Facebook } from "@styled-icons/fa-brands/FacebookSquare"
+import { Linkedin } from "@styled-icons/fa-brands/Linkedin"
+import { TwitterSquare as Twitter } from "@styled-icons/fa-brands/TwitterSquare"
+
+const socialIcons = [Facebook, Twitter, Linkedin]
+export default socialIcons
